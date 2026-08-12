cae1ec8b7c5d38a7fefd869dc20cac1def94b556
Govind Tiwari <108tiwari.g@gmail.com>
2026-08-13 00:20:21 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.go
@@ -0,0 +1,23 @@
+func getConcatenation(nums []int) []int {
+	// n := len(nums)
+
+    // ans := make([]int, n)
+
+	// copy(ans, nums)
+
+	// for i:=0; i<n; i++ {
+	// 	ans = append(ans, nums[i])
+	// }
+	// return ans
+
+
+	// Even simpler:
+	// ans := append([]int{}, nums...)
+    // ans = append(ans, nums...)
+    // return ans
+
+
+
+	// cleanest
+	return append(nums, nums...)
+}
